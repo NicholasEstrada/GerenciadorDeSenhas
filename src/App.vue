70b181eab9baba156e4d006e692055a1a5a4c6a7
Nicholas Estrada <nicholas26cervatti@gmail.com>
2021-08-18 01:34:36 -0300

    proj inicial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container" id="app">
+    <h1>{{ titulo }}</h1>
+    <h2>Vai nanar Relen</h2>
+    <div class="input-group">
+      <input type="text" placeholder="senha" v-model="novaSenha">
+      <input type="text" placeholder="user" v-model="novoUser">
+      <input type="text" placeholder="url" v-model="novoUrl">
+      <span class="input-group-btn">
+       <button @click="adcSenha" class="btn btn-success" >Adicionar</button>
+      </span>
+    </div>
+
+    <div class="conteiner">
+      <ul>
+        <li v-for="(site, index) in sites" :key="index">
+          <label>{{site.user}} </label>
+          <label>{{site.senha}} </label>
+          <label>{{site.url}} </label>
+        </li>
+      </ul>
+    </div>
+
+    <div class="footer">
+      <hr>
+      <em>Tipo do seu site</em>
+      <input type="text" v-model="titulo">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      titulo: "Minhas Senhas",
+      novaSenha: "",
+      novoUser: "",
+      novoUrl: "",
+      sites:[
+        {senha: "senha", user: "user", url: "https://google.com"},
+      ]
+    }
+  },
+  methods:{
+  adcSenha(){
+    this.sites.push({
+      senha: this.novaSenha,
+      user: this.novoUser,
+      url: this.novoUrl
+    });
+    this.novaSenha = "";
+    this.novoUser = "";
+    this.novoUrl = "";
+  }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
